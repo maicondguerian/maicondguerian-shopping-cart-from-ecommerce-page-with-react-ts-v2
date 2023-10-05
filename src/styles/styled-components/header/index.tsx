0d42a1cdd4840eecae960274a6670f034c62a457
maicondguerian <maicondguerian@gmail.com>
2023-10-05 14:38:57 -0300

    adicioando responsonsividade ao header

--- src/styles/styled-components/header/index.tsx
+++ src/styles/styled-components/header/index.tsx
@@ -3,12 +3,17 @@ import styled from 'styled-components'
 export const Header = styled.header`
     display: flex;
     justify-content: space-around;
-    padding: 2rem 0 .2rem ;
+    position: relative;
 
-    >ul{
+    padding: 2rem 0 .2rem;
+
+    a{
+        padding: .2rem .5rem;
+    }
+    
+    ul{
         display: flex;
         gap: 2rem;
-        
         
         >li{
             text-transform: capitalize;
@@ -17,39 +22,23 @@ export const Header = styled.header`
             justify-content: center;
             padding:  0  0 1.5rem;
             color: ${({ theme }) => theme.colors.darkGrayishBlue};
-
-            &:first-child{
-                margin-right: 2.5rem;
-            }
+            border-bottom: solid 4px transparent;
         }
 
         .currentPage{
-            bottom: 0;
-            box-shadow: 0  4px ${({ theme }) => theme.colors.orange};
+            border-bottom: solid 4px ${({ theme }) => theme.colors.orange};
             color: ${({ theme }) => theme.colors.veryDarkBlue};
         }
     }
 
     ul:nth-child(2){
-        li:nth-child(1){ 
-           margin: 1rem;
-        }
-        li:nth-child(2){ 
-            img{
-                height: 50px;
-                border: solid 2px transparent;
-                border-radius: 50%;
-                cursor: pointer;
-                &:hover{
-                    border: solid 2px ${({ theme }) => theme.colors.orange};
-                    transition: all .2s ease-in-out;
-                }
-            }
-        }
+        margin-left: 2rem;
     }
 
-    a{
-        padding: .2rem .5rem;
+    @media screen and (max-width: 900px){
+        ul:nth-child(2){
+            display: none;
+        }
+        
     }
-    
 `;
--- src/ui/components/header/index.tsx
+++ src/ui/components/header/index.tsx
@@ -5,28 +5,58 @@ import { BsCart2 } from 'react-icons/bs';
 import Separator from "../separador";
 import { Link } from "react-router-dom";
 import usePath from "../../../data/custom-hooks/usePath";
+import styled from "styled-components";
 
 export default function Header() {
     const { isCurrentPath } = usePath();
 
-    console.log(isCurrentPath('/') ? 's' : 'n');
+    const inlineStyle = {
+        display: 'flex',
+        justifyContent: 'space-around',
+    };
+
+    const StyledUl = styled.ul`
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+
+        li:nth-child(2){ 
+            img{
+                height: 50px;
+                border: solid 2px transparent;
+                border-radius: 50%;
+                cursor: pointer;
+
+                &:hover{
+                    border: solid 2px  ${({ theme }) => theme.colors.orange};
+                    transition: all .2s ease-in-out;
+                }
+            }
+        }
+
+    `;
 
     return (
         <>
-            <Styled.Header>
-                <ul>
-                    <li><Link to={'/'}><Logo /></Link></li>
-                    <li className={isCurrentPath('/collections') ? 'currentPage' : 'regularStyle'}><Link to={'/collections'}> collections</Link></li>
-                    <li>men</li>
-                    <li>woman</li>
-                    <li>about us</li>
-                    <li>contact</li>
-                </ul>
-                <ul>
+            <div style={inlineStyle}>
+                <Styled.Header>
+                    <ul>
+                        <li>hamb</li>
+                        <li><Link to={'/'}><Logo /></Link></li>
+                    </ul>
+                    <ul>
+                        <li className={isCurrentPath('/collections') ? 'currentPage' : 'regularStyle'}><Link to={'/collections'}> collections</Link></li>
+                        <li>men</li>
+                        <li>woman</li>
+                        <li>about us</li>
+                        <li>contact</li>
+                    </ul>
+                </Styled.Header>
+                <StyledUl>
                     <li><BsCart2 size={'25px'} /></li>
                     <li><ProfileAvatarImage /></li>
-                </ul>
-            </Styled.Header>
+                </StyledUl>
+            </div>
             <Separator width={'90%'} />
         </>
     )
