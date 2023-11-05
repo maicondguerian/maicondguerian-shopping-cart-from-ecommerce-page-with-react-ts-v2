f298fd692724907660129a87e89d0181ea8fb9b4
maicondguerian <maicondguerian@gmail.com>
2023-11-05 15:00:09 -0300

    fixes to deploy in vercel path into src

--- src/ui/components/product-iimages/gallery-images.ts
+++ src/ui/components/product-iimages/gallery-images.ts
@@ -1,11 +1,11 @@
-import thumbnail1 from "@/assets/image-product-1-thumbnail.jpg";
-import thumbnail2 from "@/assets/image-product-2-thumbnail.jpg";
-import thumbnail3 from "@/assets/image-product-3-thumbnail.jpg";
-import thumbnail4 from "@/assets/image-product-4-thumbnail.jpg";
-import productFullSize1 from "@/assets/image-product-1.jpg";
-import productFullSize2 from "@/assets/image-product-2.jpg";
-import productFullSize3 from "@/assets/image-product-3.jpg";
-import productFullSize4 from "@/assets/image-product-4.jpg";
+import thumbnail1 from "@src/assets/image-product-1-thumbnail.jpg";
+import thumbnail2 from "@src/assets/image-product-2-thumbnail.jpg";
+import thumbnail3 from "@src/assets/image-product-3-thumbnail.jpg";
+import thumbnail4 from "@src/assets/image-product-4-thumbnail.jpg";
+import productFullSize1 from "@src/assets/image-product-1.jpg";
+import productFullSize2 from "@src/assets/image-product-2.jpg";
+import productFullSize3 from "@src/assets/image-product-3.jpg";
+import productFullSize4 from "@src/assets/image-product-4.jpg";
 
 export const galleryImages = [
     {
